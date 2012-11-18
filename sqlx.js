f6c8bbae67b078372e688b33662ae9fa2f76bd57
Thomas Reinberger <treinberger@appcelerator.com>
2012-11-18 09:45:30 +0100

    added method addTableColumns for ALTER TABLE ADD COLUMN migration

--- sqlx.js
+++ sqlx.js
@@ -2,27 +2,25 @@
  * Local SQLite sync adapter which will store all models in
  * an on device database
  */
-var _ = require('alloy/underscore')._, 
-	db;
+var _ = require('alloy/underscore')._, db;
 
 function S4() {
-   return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+	return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
 };
 
 function guid() {
-   return (S4()+S4()+'-'+S4()+'-'+S4()+'-'+S4()+'-'+S4()+S4()+S4());
-};	
+	return (S4() + S4() + '-' + S4() + '-' + S4() + '-' + S4() + '-' + S4() + S4() + S4());
+};
 
 function InitAdapter(config) {
 	if (!db) {
 		if (Ti.Platform.osname === 'mobileweb' || typeof Ti.Database === 'undefined') {
 			throw 'No support for Titanium.Database in MobileWeb environment.';
-		}
-		else {
+		} else {
 			db = Ti.Database.open('_alloy_');
 		}
 		module.exports.db = db;
-		
+
 		// create the migration table in case it doesn't exist
 		db.execute('CREATE TABLE IF NOT EXISTS migrations (latest TEXT, model TEXT)');
 	}
@@ -42,77 +40,80 @@ function GetMigrationFor(table) {
 
 function SQLiteMigrateDB() {
 	//TODO: normalize columns at compile time - https://jira.appcelerator.org/browse/ALOY-222
-	this.column = function(name)
-	{
-		switch(name)
-		{
+	this.column = function(name) {
+		switch(name) {
 			case 'string':
 			case 'varchar':
-			case 'text':
-			{
+			case 'text': {
 				return 'TEXT';
 			}
 			case 'int':
 			case 'tinyint':
 			case 'smallint':
 			case 'bigint':
-			case 'integer':
-			{
+			case 'integer': {
 				return 'INTEGER';
 			}
 			case 'double':
 			case 'float':
-			case 'real':
-			{
+			case 'real': {
 				return 'REAL';
 			}
-			case 'blob':
-			{
+			case 'blob': {
 				return 'BLOB';
 			}
 			case 'decimal':
 			case 'number':
 			case 'date':
 			case 'datetime':
-			case 'boolean':
-			{
+			case 'boolean': {
 				return 'NUMERIC';
 			}
-			case 'null':
-			{
+			case 'null': {
 				return 'NULL';
 			}
 		}
 		return 'TEXT';
 	};
-	
-	this.createTable = function(name,config) {
-		Ti.API.info('create table migration called for '+config.adapter.tablename);
-		
-		var self = this,
-			columns = [];
-			
+
+	this.createTable = function(config) {
+		Ti.API.info('create table migration called for ' + config.adapter.collection_name);
+
+		var self = this, columns = [];
+
 		for (var k in config.columns) {
-			columns.push(k+" "+self.column(config.columns[k]));
+			columns.push(k + " " + self.column(config.columns[k]));
 		}
-			
-		var sql = 'CREATE TABLE '+config.adapter.tablename+' ( '+columns.join(',')+',id' + ' )';
+
+		var sql = 'CREATE TABLE ' + config.adapter.collection_name + ' ( ' + columns.join(',') + ',id' + ' )';
 		Ti.API.info(sql);
-		
+
 		db.execute(sql);
 	};
-	
+
+	this.addTableColumns = function(config) {
+		Ti.API.info("add table columns migration called for " + config.adapter.collection_name);
+		var self = this;
+
+		for (var k in config.columns) {
+
+			var sql = "ALTER TABLE " + config.adapter.collection_name + " ADD COLUMN " + k + " " + self.column(config.columns[k]);
+			Ti.API.info(sql);
+			db.execute(sql);
+		}
+	};
+
 	this.dropTable = function(name) {
-		Ti.API.info('drop table migration called for '+name);
-		db.execute('DROP TABLE IF EXISTS '+name);
+		Ti.API.info('drop table migration called for ' + name);
+		db.execute('DROP TABLE IF EXISTS ' + name);
 	};
 }
 
 function _valueType(value) {
-	if (typeof value == 'string') {
-		return "'"+value+"'";
+	if ( typeof value == 'string') {
+		return "'" + value + "'";
 	}
-	if (typeof value == 'boolean') {
+	if ( typeof value == 'boolean') {
 		return value ? 1 : 0;
 	}
 
@@ -123,44 +124,40 @@ function _buildQuery(table, opts) {
 	var sql = 'SELECT * ';
 	if (opts.select) {
 		sql = 'SELECT ';
-		if (typeof opts.select == 'array') {
+		if ( typeof opts.select == 'array') {
 			sql += opts.select.join(", ");
-		}
-		else {
+		} else {
 			sql += opts.select;
 		}
 	}
 
-	sql += ' FROM '+table;
+	sql += ' FROM ' + table;
 
 	if (opts.where) {
 		var where;
-		if (typeof opts.where === 'object') {
+		if ( typeof opts.where === 'object') {
 			where = [];
 			_.each(opts.where, function(v, f) {
-				where.push(f+" = "+_valueType(v));
+				where.push(f + " = " + _valueType(v));
 			});
 			where = where.join(' AND ');
-		}
-		else if (typeof opts.where === 'array') {
+		} else if ( typeof opts.where === 'array') {
 			where = opts.where.join(' AND ');
-		}
-		else {
+		} else {
 			where = opts.where;
 		}
 
-		sql += ' WHERE '+ where;
+		sql += ' WHERE ' + where;
 	}
 	if (opts.orderBy) {
 		var order;
-		if (typeof opts.orderBy === 'array') {
+		if ( typeof opts.orderBy === 'array') {
 			order = opts.orderBy.join(', ');
-		}
-		else {
+		} else {
 			order = opts.orderBy;
 		}
 
-		sql += ' ORDER BY '+ order;
+		sql += ' ORDER BY ' + order;
 	}
 	if (opts.limit) {
 		sql += ' LIMIT ' + opts.limit;
@@ -169,26 +166,26 @@ function _buildQuery(table, opts) {
 		}
 	}
 	if (opts.union) {
-		sql += ' UNION '+ _buildQuery(opts.union);
+		sql += ' UNION ' + _buildQuery(opts.union);
 	}
 	if (opts.unionAll) {
-		sql += ' UNION ALL '+ _buildQuery(opts.unionAll);
+		sql += ' UNION ALL ' + _buildQuery(opts.unionAll);
 	}
 	if (opts.intersect) {
-		sql += ' INTERSECT '+ _buildQuery(opts.intersect);
+		sql += ' INTERSECT ' + _buildQuery(opts.intersect);
 	}
 	if (opts.except) {
-		sql += ' EXCEPT '+ _buildQuery(opts.EXCEPT);
+		sql += ' EXCEPT ' + _buildQuery(opts.EXCEPT);
 	}
 
 	return sql;
 }
 
 function Sync(model, method, opts) {
-	var table =  model.config.adapter.collection_name;
+	var table = model.config.adapter.collection_name;
 	var columns = model.config.columns;
 	var resp = null;
-	
+
 	switch (method) {
 
 		case 'create':
@@ -201,7 +198,7 @@ function Sync(model, method, opts) {
 				q.push('?');
 			}
 			var id = guid();
-			var sql = 'INSERT INTO '+table+' ('+names.join(',')+',id) VALUES ('+q.join(',')+',?)';
+			var sql = 'INSERT INTO ' + table + ' (' + names.join(',') + ',id) VALUES (' + q.join(',') + ',?)';
 			values.push(id);
 			db.execute(sql, values);
 			model.id = id;
@@ -211,22 +208,20 @@ function Sync(model, method, opts) {
 		case 'read':
 			Ti.API.info("[SQLITE] opts: " + JSON.stringify(opts));
 			var sql = _buildQuery(table, opts.data || opts);
-			Ti.API.info("[SQLITE] "+ sql);
+			Ti.API.info("[SQLITE] " + sql);
 			var rs = db.execute(sql);
 			var len = 0;
-			
+
 			model.models.length = 0;
-			while(rs.isValidRow())
-			{
+			while (rs.isValidRow()) {
 				var o = {};
-                var fc = 0;
-                if (Ti.Platform.osname === 'android') {
-                    fc = rs.fieldCount;
-                }
-                else {
-                	 fc = rs.fieldCount();
-                }
-				_.times(fc,function(c){
+				var fc = 0;
+				if (Ti.Platform.osname === 'android') {
+					fc = rs.fieldCount;
+				} else {
+					fc = rs.fieldCount();
+				}
+				_.times(fc, function(c) {
 					var fn = rs.fieldName(c);
 					o[fn] = rs.fieldByName(fn);
 				});
@@ -240,39 +235,39 @@ function Sync(model, method, opts) {
 			//model.trigger('fetch');
 			len === 1 ? resp = model.models[0] : resp = model.models;
 			break;
-	
+
 		case 'update':
 			var names = [];
 			var values = [];
 			var q = [];
-			for (var k in columns)
-			{
-				names.push(k+'=?');
+			for (var k in columns) {
+				names.push(k + '=?');
 				values.push(model.get(k));
 				q.push('?');
 			}
-			var sql = 'UPDATE '+table+' SET '+names.join(',')+' WHERE id=?';
-			
-		    var e = sql +','+values.join(',')+','+model.id;
-		    values.push(model.id);
-			db.execute(sql,values);
+			var sql = 'UPDATE ' + table + ' SET ' + names.join(',') + ' WHERE id=?';
+
+			var e = sql + ',' + values.join(',') + ',' + model.id;
+			values.push(model.id);
+			db.execute(sql, values);
 			resp = model.toJSON();
 			break;
 
 		case 'delete':
-			var sql = 'DELETE FROM '+table+' WHERE id=?';
+			var sql = 'DELETE FROM ' + table + ' WHERE id=?';
 			db.execute(sql, model.id);
 			model.id = null;
 			resp = model.toJSON();
 			break;
 	}
 	if (resp) {
-        opts.success(resp);
-        method === "read" && model.trigger("fetch");
-    } else opts.error("Record not found");
+		opts.success(resp);
+		method === "read" && model.trigger("fetch");
+	} else
+		opts.error("Record not found");
 }
 
-function GetMigrationForCached(t,m) {
+function GetMigrationForCached(t, m) {
 	if (m[t]) {
 		return m[t];
 	}
@@ -286,20 +281,21 @@ function GetMigrationForCached(t,m) {
 function Migrate(migrations) {
 	var prev;
 	var sqlMigration = new SQLiteMigrateDB;
-	var migrationIds = {}; // cache for latest mid by model name
-	
+	var migrationIds = {};
+	// cache for latest mid by model name
+
 	db.execute('BEGIN;');
-	
+
 	// iterate through all our migrations and call up/down and the last migration should
 	// have the up called but never the down -- the migrations come in pre sorted from
 	// oldest to newest based on timestamp
-	_.each(migrations,function(migration) {
+	_.each(migrations, function(migration) {
 		var mctx = {};
 		migration(mctx);
-		var mid = GetMigrationForCached(mctx.name,migrationIds);
-		Ti.API.info('mid = '+mid+', name = '+mctx.name);
+		var mid = GetMigrationForCached(mctx.name, migrationIds);
+		Ti.API.info('mid = ' + mid + ', name = ' + mctx.name);
 		if (!mid || mctx.id > mid) {
-			Ti.API.info('Migration starting to '+mctx.id+' for '+mctx.name);
+			Ti.API.info('Migration starting to ' + mctx.id + ' for ' + mctx.name);
 			if (prev && _.isFunction(prev.down)) {
 				prev.down(sqlMigration);
 			}
@@ -308,18 +304,17 @@ function Migrate(migrations) {
 				mctx.up(sqlMigration);
 			}
 			prev = mctx;
-		}
-		else {
-			Ti.API.info('skipping migration '+mctx.id+', already performed');
+		} else {
+			Ti.API.info('skipping migration ' + mctx.id + ', already performed');
 			prev = null;
 		}
 	});
-	
+
 	if (prev && prev.id) {
 		db.execute('DELETE FROM migrations where model = ?', prev.name);
-		db.execute('INSERT INTO migrations VALUES (?,?)', prev.id,prev.name);
+		db.execute('INSERT INTO migrations VALUES (?,?)', prev.id, prev.name);
 	}
-	
+
 	db.execute('COMMIT;');
 }
 
@@ -328,7 +323,7 @@ module.exports.sync = Sync;
 module.exports.beforeModelCreate = function(config) {
 	config = config || {};
 
-    InitAdapter(config);
+	InitAdapter(config);
 
 	return config;
 };
@@ -336,8 +331,8 @@ module.exports.beforeModelCreate = function(config) {
 module.exports.afterModelCreate = function(Model) {
 	Model = Model || {};
 
-	
-	Model.prototype.config.Model = Model; // needed for fetch operations to initialize the collection from persistent store
+	Model.prototype.config.Model = Model;
+	// needed for fetch operations to initialize the collection from persistent store
 
 	Migrate(Model.migrations);
 
